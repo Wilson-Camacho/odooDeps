c4e017c5d109894a9ad3497356c42359933119fc
Tu Nombre <naovilla2002@gmail.com>
2022-02-11 09:47:31 +0100

    Terminado nueva vista.

--- odoo14-custom-addons/lista_tareas/models/models.py
+++ odoo14-custom-addons/lista_tareas/models/models.py
@@ -7,11 +7,15 @@ class lista_tareas(models.Model):
     _name = 'lista_tareas.lista_tareas'
     _description = 'lista_tareas.lista_tareas'
 
-   
+
+    avatar = fields.Image("Imagen tarea", max_width=50, max_height = 50)
+    
     tarea = fields.Char()
     prioridad = fields.Integer()
     urgente = fields.Boolean(compute="_value_urgente", store=True)
     realizada = fields.Boolean()
+    
+    fecha = fields.Date()
 
     @api.depends('prioridad')
     
@@ -19,7 +23,7 @@ class lista_tareas(models.Model):
     
         for record in self:
     
-            if record.prioridad>10:
+            if record.prioridad > 8:
                 record.urgente = True
             else:
                 record.urgente = False
